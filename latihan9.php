377ac3d1def130d437c8bd1b58f69c94b13c306f
marlingga <145709139+marlingga@users.noreply.github.com>
2023-11-17 00:05:30 +0700

    Add files via upload

---
+++ latihan9.php
@@ -0,0 +1,18 @@
+<?php 
+$a = "hello";
+$hello = "hello world!";
+
+// menampilkan isi variabel $a
+// hello
+echo $a . "</br>";
+
+// menampilkan isi variabel $a
+// hello world
+echo $hello . "</br>";
+
+// menampilkan isi dari variabel dengan nama yang sama seperti isi variabel $a
+// isi variabel $a = hello. jadi, nanti akan menampilkan isi dari variabel $hello
+// hello world
+echo $$a . "</br>";
+
+?>
