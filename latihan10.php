3d4605119576655aa74409410a24cf092f3fa645
marlingga <145709139+marlingga@users.noreply.github.com>
2023-11-17 00:07:13 +0700

    Add files via upload

---
+++ latihan10.php
@@ -0,0 +1,14 @@
+<?php 
+
+$nrp= "173040888";
+$nama = "lorem ipsum";
+$umur = 20;
+$nilai = 75.75;
+
+echo "nrp :". $nrp . "</br>";
+echo "nama : $nama</br>";
+print "umur : ".$umur; print "</br>";
+// menampilkan bilangan real dengan format 3 digit dibelakang koma
+printf ("nilai : %.3f</br>", $nilai);
+
+?>
