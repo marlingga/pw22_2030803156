527996abdd3dfd7b13861c37b89985091e15139a
marlingga <145709139+marlingga@users.noreply.github.com>
2023-12-08 14:48:11 +0700

    Add files via upload

---
+++ latihan18.php
@@ -0,0 +1,24 @@
+<?php
+$mahasiswa = ["joni","123456","sistem informasi","dyan@gmail.com"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>Document</title>    
+</head>
+
+<body>
+<h1> Daftar Mahasiswa </h1>    
+<ul>
+<li>joni</li>
+<li>123456</li>
+<li>sistem informasi</li>   
+<li>dyan@gmail.com</li>
+
+</ul>
+
+</body>
+    
+</html>
